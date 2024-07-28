d64dd7b9f44b67303f3a4429c2184be8f5ab3697
Tooclose21 <alasuper.as@gmail.com>
2024-07-28 13:13:15 +0200

    Minor changes

--- app/src/main/java/com/example/apptest4/FinishedGameActivity.kt
+++ app/src/main/java/com/example/apptest4/FinishedGameActivity.kt
@@ -6,8 +6,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-
-
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.systemBarsPadding
